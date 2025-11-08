4f4291885d002c79b0d77edd8c4568aac82e5968
SSK <shrishask884@gmail.com>
2025-11-08 17:05:44 +0530

    Operations_firstpart

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,24 @@
 -- Main entery point for the app
-local services = require("src.services")
+local io_services = require("src.services.io_service")
+local op_services = require("src.services.operations")
 
 function Main()
     print("Welcome to Lua todo manager!")
-    services.io_service.display_help()
+    io_services.display_help()
     while true do
-        local user_input = services.io_service.take_str_input()
+        local user_input = io_services.take_str_input()
         if user_input:find("^read") then
-            services.operations:read_todo()
+            op_services.operations:read_todo(user_input)
         elseif user_input:find("^delete") then
-            services.operations:delete_todo()
+            op_services.operations:delete_todo(user_input)
         elseif user_input:find("^update") then
-            services.operations:update_todo()
+            op_services.operations:update_todo(user_input)
         elseif user_input == "help" then
-            services.io_service.display_help()
+            io_services.display_help()
         elseif user_input == "todos" then
-            services.operations:list_todos()
-        elseif user_input == "create" then
-            services.operations:create_todo()
+            op_services.operations:list_todos()
+        elseif user_input:find("^create") then
+            op_services.create_todo(user_input)
         elseif user_input == "exit" then
             break
         else
